4576ae95cf40ff662b4c217ec89fe97a9d7978e3
eslahkonha.mona@gmail.com <eslahkonha.mona@gmail.com>
2024-11-20 16:21:02 +0100

    login static

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+  const [token, setToken] = useState(null)
+  const login = () => {
+    const userData = {
+      email: 'admin@gmail.com',
+      username: 'Mona',
+      password: 'I123456Ma'
+    }
+    axios
+      .post('http://localhost:5005/auth/login', userData)
+      .then(res => {
+        const authToken = res.data.authToken
+        setToken(authToken)
+        localStorage.setItem('authToken', authToken)
+      })
 
+      .catch(e => {
+        console.log(e)
+      })
+  }
+  useEffect(() => {
+    const savedToken = localStorage.getItem('authToken')
+    if (savedToken) {
+      setToken(savedToken)
+    }
+  }, [])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={login}>Login</button>
     </>
   )
 }
