6258e47da44df38b43edbf9c706500c78e2a4d9e
eslahkonha.mona@gmail.com <eslahkonha.mona@gmail.com>
2024-11-21 18:17:20 +0100

    update navbar items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,41 @@
-import React from 'react';
-import '../App.css';
-import { NavbarData } from './NavbarData';
+import React from 'react'
 
-function Navbar() {
+import { NavbarData } from './NavbarData'
+import { useAuthContext } from '../context/authContext'
+
+function Navbar () {
+  const { user, logout } = useAuthContext()
+
+  const filteredNavbarData = NavbarData.filter(item => {
+    if (user) {
+      return item.title !== 'Sign Up' && item.title !== 'Login'
+    } else {
+      return item.title !== 'Logout' && !item.protected
+    }
+  })
+
   return (
     <nav>
       <h1>DreamCatcher</h1>
-      <ul className="navbar-links">
-        {NavbarData.map((item, index) => (
-          <li key={index} className="navbar-item">
-            <a href={item.link} className="navbar-link">
-              {item.icon}
-              <span>{item.title}</span>
-            </a>
+      <ul className='navbar-links'>
+        {filteredNavbarData.map((item, index) => (
+          <li key={index} className='navbar-item'>
+            {item.link === '/logout' ? (
+              <button onClick={logout} className='navbar-link'>
+                {item.icon}
+                <span>{item.title}</span>
+              </button>
+            ) : (
+              <a href={item.link} className='navbar-link'>
+                {item.icon}
+                <span>{item.title}</span>
+              </a>
+            )}
           </li>
         ))}
       </ul>
     </nav>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
--- src/components/NavbarData.jsx
+++ src/components/NavbarData.jsx
@@ -1,32 +1,36 @@
-import React from "react";
-import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
-import AddBoxIcon from '@mui/icons-material/AddBox';
-import NightsStayIcon from '@mui/icons-material/NightsStay';
-
-
-
-
-
-
+import NightsStayIcon from '@mui/icons-material/NightsStay'
+import AddBoxIcon from '@mui/icons-material/AddBox'
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom'
 
 export const NavbarData = [
-    {
-        title: "Your Dashboard",
-        icon: <NightsStayIcon />,
-        link: "/your-dreams",
-    },
-    
-    {
-        title: "New Dream",
-        icon: <AddBoxIcon />,
-        link: "/dream"
-        
-    },
-   
-    {
-        title: "Logout",
-        icon: <MeetingRoomIcon />,
-        link: "/logout"
-        
-    }
+  {
+    title: 'Your Dashboard',
+    icon: <NightsStayIcon />,
+    link: '/dashboard',
+    protected: true
+  },
+  {
+    title: 'New Dream',
+    icon: <AddBoxIcon />,
+    link: '/dream',
+    protected: true
+  },
+  {
+    title: 'Logout',
+    icon: <MeetingRoomIcon />,
+    link: '/logout',
+    protected: true
+  },
+  {
+    title: 'Login',
+    icon: null,
+    link: '/',
+    protected: false
+  },
+  {
+    title: 'Sign Up',
+    icon: null,
+    link: '/signup',
+    protected: false
+  }
 ]
