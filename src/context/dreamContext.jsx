37ddcb2bc2373dcd4c6298a91b12e5b66dc68942
eslahkonha.mona@gmail.com <eslahkonha.mona@gmail.com>
2024-11-22 13:16:43 +0100

    public dream started

--- src/context/dreamContext.jsx
+++ src/context/dreamContext.jsx
@@ -1,121 +1,129 @@
-import { createContext, useContext, useState, useEffect } from 'react';
-import axios from 'axios';
-import { useAuthContext } from './authContext';
+import { createContext, useContext, useState, useEffect } from 'react'
+import axios from 'axios'
+import { useAuthContext } from './authContext'
 
-const DreamContext = createContext();
+const DreamContext = createContext()
 
-export const useDreamContext = () => useContext(DreamContext);
+export const useDreamContext = () => useContext(DreamContext)
 
 export const DreamProvider = ({ children }) => {
-    const { token } = useAuthContext();
-    const [myDreams, setMyDreams] = useState([]);
-    const [publicDreams, setPublicDreams] = useState([]);
-    const [error, setError] = useState(null); // Handle errors
+  const { token } = useAuthContext()
+  const [myDreams, setMyDreams] = useState([])
+  const [publicDreams, setPublicDreams] = useState([])
+  const [error, setError] = useState(null) // Handle errors
 
-    // fetch user dreams
-    const fetchMyDreams = async () => {
-        setError(null); // Reset error state
+  // fetch user dreams
+  const fetchMyDreams = async () => {
+    setError(null) // Reset error state
 
-        try {
-            const { data } = await axios.get('http://localhost:5005/dreams/mine', {
-                headers: { Authorization: `Bearer ${token}` },
-            });
-            setMyDreams(data);
-        } catch (error) {
-            setError('Oh no, failed to fetch your dreams!');
-            console.error('Error fetching dreams:', err);
+    try {
+      const { data } = await axios.get('http://localhost:5005/dreams/mine', {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      setMyDreams(data)
+    } catch (error) {
+      setError('Oh no, failed to fetch your dreams!')
+      console.error('Error fetching dreams:', err)
+    }
+  }
+
+  // fetch public dreams
+
+  const fetchPublicDreams = async () => {
+    setError(null)
+
+    try {
+      const { data } = await axios.get('http://localhost:5005/dreams/public', {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      setPublicDreams(data)
+    } catch (error) {
+      setError('Oh no, failed to fetch public dreams!')
+      console.error('Error fetching dreams:', err)
+    }
+  }
+
+  // create new dream
+
+  const createDream = async dreamData => {
+    setError(null)
+
+    try {
+      const { data } = await axios.post(
+        'http://localhost:5005/dreams',
+        dreamData,
+        {
+          headers: { Authorization: `Bearer ${token}` }
         }
-    };
+      )
+      setMyDreams(prevDreams => [...prevDreams, data.dream]) // to add the new dream to "myDreams"
+    } catch (err) {
+      setError('Failed to create dream.')
+      console.error('Error creating dream:', err)
+    }
+  }
 
-    // fetch public dreams
+  // update existing dream
 
-    const fetchPublicDreams = async () => {
-        setError(null);
+  const updateDream = async (dreamId, updatedData) => {
+    setError(null)
 
-        try {
-            const { data } = await axios.get('http://localhost:5005/dreams/public', {
-                headers: { Authorization: `Bearer ${token}` },
-            });
-            setPublicDreams(data);
-        } catch (error) {
-            setError('Oh no, failed to fetch public dreams!');
-            console.error('Error fetching dreams:', err);
+    try {
+      const { data } = await axios.put(
+        `http://localhost:5005/dreams/${dreamId}`,
+        updatedData,
+        {
+          headers: { Authorization: `Bearer ${token}` }
         }
-    };
+      )
+      setMyDreams(prevDreams =>
+        prevDreams.map(dream => (dream._id === dreamId ? data : dream))
+      )
+    } catch (err) {
+      setError('Oh no, failed to update dream.')
+      console.error('Error updating dream:', err)
+    }
+  }
 
-    // create new dream
+  // delete a dream
 
-    const createDream = async (dreamData) => {
-        setError(null);
+  const deleteDream = async dreamId => {
+    setError(null)
 
-        try {
-            const { data } = await axios.post('http://localhost:5005/dreams', dreamData, {
-                headers: { Authorization: `Bearer ${token}` },
-            });
-            setMyDreams((prevDreams) => [...prevDreams, data.dream]); // to add the new dream to "myDreams"
-        } catch (err) {
-            setError('Failed to create dream.');
-            console.error('Error creating dream:', err);
-        }
-    };
+    try {
+      await axios.delete(`http://localhost:5005/dreams/${dreamId}`, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      setMyDreams(prevDreams =>
+        prevDreams.filter(dream => dream._id !== dreamId)
+      )
+    } catch (err) {
+      setError('Oh no, failed to delete dream.')
+      console.error('Error deleting dream:', err)
+    }
+  }
 
-    // update existing dream
+  useEffect(() => {
+    if (token) {
+      fetchMyDreams()
+      fetchPublicDreams()
+    }
+  }, [token])
 
-    const updateDream = async (dreamId, updatedData) => {
-        setError(null);
-
-        try {
-            const { data } = await axios.put(`http://localhost:5005/dreams/${dreamId}`, updatedData, {
-                headers: { Authorization: `Bearer ${token}` },
-            });
-            setMyDreams((prevDreams) =>
-                prevDreams.map((dream) => (dream._id === dreamId ? data : dream))
-            );
-        } catch (err) {
-            setError('Oh no, failed to update dream.');
-            console.error('Error updating dream:', err);
-        }
-    };
-
-    // delete a dream
-
-    const deleteDream = async (dreamId) => {
-        setError(null);
-
-        try {
-            await axios.delete(`http://localhost:5005/dreams/${dreamId}`, {
-                headers: { Authorization: `Bearer ${token}` },
-            });
-            setMyDreams((prevDreams) => prevDreams.filter((dream) => dream._id !== dreamId));
-        } catch (err) {
-            setError('Oh no, failed to delete dream.');
-            console.error('Error deleting dream:', err);
-        }
-    };
-
-
-    useEffect(() => {
-        if (token) {
-            fetchMyDreams();
-            fetchPublicDreams();
-        }
-    }, [token]);
-
-    return (
-        <DreamContext.Provider
-          value={{
-            myDreams,
-            publicDreams,
-            fetchMyDreams,
-            fetchPublicDreams,
-            createDream,
-            updateDream,
-            deleteDream,
-            error,
-            
-          }}
-        >
-          {children}
-        </DreamContext.Provider>
-      );
-    };
+  return (
+    <DreamContext.Provider
+      value={{
+        myDreams,
+        publicDreams,
+        fetchMyDreams,
+        fetchPublicDreams,
+        createDream,
+        updateDream,
+        deleteDream,
+        error
+      }}
+    >
+      {children}
+    </DreamContext.Provider>
+  )
+}
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,19 +3,17 @@ import Login from '../pages/Login'
 import SignUp from '../pages/signup'
 import CreateD from '../pages/createDream'
 import YourDreams from '../pages/displayDreams'
+import PublicDreams from '../pages/displayDreams'
 
 const AppRoutes = () => {
   return (
-
-    
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/dream' element={<CreateD />} />
-        <Route path='/your-dreams' element={<YourDreams />} />
-      </Routes>
-    
-
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/dream' element={<CreateD />} />
+      <Route path='/your-dreams' element={<YourDreams />} />
+      <Route path='/public-dreams' element={<PublicDreams />} />
+    </Routes>
   )
 }
 
